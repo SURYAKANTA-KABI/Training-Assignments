426a29d77bce67818262091db86485b04ca0a7ac
Suryakanta <75796406+SURYAKANTA-KABI@users.noreply.github.com>
2022-03-14 11:33:28 +0530

    Added Spring IOC concepts

---
+++ springioc/src/com/cg/ioc/Sim.java
@@ -0,0 +1,7 @@
+package com.cg.ioc;
+
+public interface Sim {
+	void calling();
+	void data();
+
+}
---
+++ springioc/src/com/cg/ioc/Vodafone.java
@@ -0,0 +1,18 @@
+package com.cg.ioc;
+
+public class Vodafone implements Sim {
+
+	@Override
+	public void calling() {
+		System.out.println("calling using vodafone sim");
+		
+	}
+
+	@Override
+	public void data() {
+		System.out.println("browsing internet using vodafone sim");
+		
+	}
+	
+
+}
