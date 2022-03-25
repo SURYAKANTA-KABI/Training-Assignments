9a430ed2ad72f9a6edb65c0cb181e9eddc7d49c4
Suryakanta <75796406+SURYAKANTA-KABI@users.noreply.github.com>
2022-03-25 16:38:17 +0530

    Added Spring security concepts

---
+++ spring-boot-security-learn/src/main/java/com/learn/config/MySecurityConfig.java
@@ -0,0 +1,42 @@
+package com.learn.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().
+		anyRequest().
+		authenticated().
+		and().
+		httpBasic();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("Surya")
+		.password("123")
+		.roles("NORMAL");
+		auth.inMemoryAuthentication()
+		.withUser("Kabi")
+		.password("1234")
+		.roles("ADMIN");
+	}
+	
+	@Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
